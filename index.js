9a07ff2735cc36ca900e2687e601c51462c8be14
thequlmatov <zuxroka0104@gmail.com>
2023-07-10 22:28:24 +0500

    3-Oy-1-dars

---
+++ index.js
@@ -0,0 +1,18 @@
+//    1 =>  vs codega user nameni tanishtirish
+// git config --global user.name ""
+// git config --global user.email ""
+
+//   2  =>   gitni o`rnatish 
+//  git init 
+
+//   3  => repositoryga yo`l
+// git remote add origin " yo`l "
+
+//  4  =>fayllarni cloudga yuklash
+//  git add .
+
+//  5 => commit
+//  git commit -m "3-Oy"
+
+//  6  => push
+//  git push origin master
